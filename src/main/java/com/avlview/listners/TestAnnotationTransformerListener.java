d94205a5337eb9c2d28a81db73c87779e90dc498
Murali Krishnan <mkrishnan@mapview.in>
2019-03-11 15:43:22 +0530

    on mar 11

--- src/main/java/com/avlview/listners/TestAnnotationTransformerListener.java
+++ src/main/java/com/avlview/listners/TestAnnotationTransformerListener.java
@@ -23,16 +23,15 @@ public class TestAnnotationTransformerListener implements IAnnotationTransformer
 		 * if (!annotation.getEnabled() && System.getenv("All").contains("false")) {
 		 * System.out.println("all"); annotation.setEnabled(false); }
 		 */
-		/*
-		 * if (testMethod.getName().equals("getvehiclemakes") &&
-		 * System.getenv("getvehiclemakesTest").contains("false")) {
-		 * System.out.println("Disable " + testMethod.getName());
-		 * annotation.setEnabled(false); } else if
-		 * (testMethod.getName().equals("getapnlistbycountryid") &&
-		 * System.getenv("getapnlistbycountryidTest").contains("false")) {
-		 * System.out.println("set data provider for " + testMethod.getName());
-		 * annotation.setEnabled(false); }
-		 */
+
+		if (testMethod.getName().equals("getvehiclemakes") && System.getenv("getvehiclemakesTest").contains("false")) {
+			System.out.println("Disable " + testMethod.getName());
+			annotation.setEnabled(false);
+		} else if (testMethod.getName().equals("getapnlistbycountryid")
+				&& System.getenv("getapnlistbycountryidTest").contains("false")) {
+			System.out.println("Disable " + testMethod.getName());
+			annotation.setEnabled(false);
+		}
 
 	}
 
